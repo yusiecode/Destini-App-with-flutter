cd0503a2f3931ff7a22ef517a7087d642b6cf800
Yousaf Ali <86404110+AndriosSoft@users.noreply.github.com>
2023-02-20 01:35:48 +0500

    first commit

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+import 'dart:core';
+
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
+}
